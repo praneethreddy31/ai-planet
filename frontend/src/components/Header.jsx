bbc9945fda539411974a93eff5379aaf7a53a812
Praneeth Reddy Karrem <praneethreddykarrem123@gmail.com>
2025-05-23 00:36:47 +0530

    added README

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -9,14 +9,8 @@ const Header = () => {
       <div className="header-content">
         <Link to="/" className="logo-container">
           <img src={logo} alt="AI Planet Logo" className="logo" />
-          <span className="logo-text">formerly DPHI</span>
         </Link>
         
-        <div className="header-actions">
-          <Link to="/" className="upload-btn">
-            <i className="fas fa-upload"></i> Upload PDF
-          </Link>
-        </div>
       </div>
     </header>
   );
